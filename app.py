1662101eb01090af36d86c1a89a4eebd27a05416
root <root@vps1706433015.tech0.ru>
2024-01-28 14:45:07 +0300

    v0.2 def reverse

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from flask_sock import Sock
 
 app = Flask(__name__)
+sock = Sock(app)
 
 @app.route('/')
 def index():
     return 'Hello, WebSockets!'
+
+@sock.route('/reverse')
+def reverse(ws):
+    while True:
+        text = ws.receive()
+        ws.send(text[::-1])
+
 
 if __name__ == '__main__':
     app.run()
